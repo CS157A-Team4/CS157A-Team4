17f1fbb7bf2cbe5f17bb1e217200b1ce0ad2b401
jvan1997 <jonathanvan.1997@gmail.com>
2019-09-07 04:42:29 -0700

    tailwindcss, routing, error page added

--- project/bookie/src/index.js
+++ project/bookie/src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import{Router, Route,Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import Error from './error';
+const browserHistory = createBrowserHistory();
+ReactDOM.render(
+    <Router path="/App" history={browserHistory}>
+    <div>
+        <Switch>
+            <Route exact path = '/' component ={App}  />
+            <Route component={Error}/>
+        </Switch>
+    </div>
+    </Router>, document.getElementById('root'));
+    
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

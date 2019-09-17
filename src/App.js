20ea6f3dbe47d8e4834c4fc86e23d42d203c3ae8
jvan1997 <jonathanvan.1997@gmail.com>
2019-09-16 17:07:15 -0700

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Home page for Bookie
+          Home page for Bookie WHYYY
         </p>
         <a
           className="App-link"

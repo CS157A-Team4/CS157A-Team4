e213f6fcfcffb3c8f1f53a15b05dc2c04efc6527
yuxiu198 <xiuyutiffany@gmail.com>
2019-09-26 00:32:30 -0700

    Uploaded part of requirements document, added new pages
    Add one directory named "freinds" and two js files named "friendsList.js" file and "friendsRequest.js" file under the directory. Add a "passwordChange.js" file to the profile directory.

---
+++ src/Pages/friends/friendsList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class FriendsList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: '',
+            lastName: '',
+        };
+    }
+    render() {
+        return(
+            <div>
+                //some elements
+            </div>
+        )
+    }
+}
+
+export default FriendsList;
---
+++ src/Pages/profile/passwordChange.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class PasswordChange extends React.Component {
+    // a constructor to construct the PasswordChange Component
+    constructor(props) {
+        super(props);
+        this.state = {
+            oldPassword: '',
+            newPassword: '',
+        };
+    }
+    // add all the features
+    render() {
+        return(
+            <div>
+                //some elements
+            </div>
+        )
+    }
+}
+
+export default PasswordChange;

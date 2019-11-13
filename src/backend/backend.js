57fb6146856a3f9586dd8c2afe2c7270a7c04884
jvan1997 <32347479+jvan1997@users.noreply.github.com>
2019-11-12 16:40:03 -0800

    Update backend.js

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -1,5 +1,5 @@
 // switch back and forth when you want to test
-//const api = "https://sjsubookiebackend.herokuapp.com";
-const api = "https://sjsubookietest.herokuapp.com";
+const api = "https://sjsubookiebackend.herokuapp.com";
+//const api = "https://sjsubookietest.herokuapp.com";
 
 export default api

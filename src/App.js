235f81790950be385c28be320591140c1b736db7
jvan1997 <jonathanvan.1997@gmail.com>
2019-11-12 19:38:08 -0800

    commit home page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { withRouter } from 'react-router-dom';
 import { thisExpression } from '@babel/types';
 import api from './backend/backend';
 import { slide as Menu } from "react-burger-menu";
-
+import background from './images/background1.png';
 class App extends React.Component {
   constructor(props) {
       super(props);
@@ -15,86 +15,59 @@ class App extends React.Component {
           email: '',
           password: '',
           secondpassword: '',
+          bookname:'',
+          course:'',
       };
-
-  
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
   }
-  UNSAFE_componentWillMount() {
-    console.log("hi");
-    fetch(api+"/testapi/tables", {
-      method: "GET",
-      headers: {
-        'accept': 'application/json',
-      }
-    })
-    .then(function (response) {
-      console.log("hi");
-        return response.json();
-    })
-    .then(function (data) {
-        if (data["error"]) {
-            this.setState({
-                error: data["message"]
-            });
-            console.log(data);
-        }
-        else {
-          console.log(data);
-            this.setState({message:data});
-        }
-
-    }.bind(this));
-    }
-    createTable = () => {
-      if(this.state.message){
-      let table = []
-      const len = this.state.message.length;
-      const message = this.state.message;
-      for(let i = 0; i< len; i++){
-        let current = message[i];
-        table.push(<p> {current['iduser']} {current['email']} {current['firstname']} {current['surname']} {current['schoolid']} </p> );
-      }
-      return table;
-    }
-  return '';
+  goTo(event) {
+    this.props.history.push(`/createPost`);
   }
-  
   handleChange(event) {
-      this.setState({
-          [event.target.id]: event.target.value
-      });
+    this.setState({
+      [event.target.id]: event.target.value
+    });
   }
-  handleSubmit(event){
-    fetch("localhost:8090/tables")
-      .then(function (response) {
-          return response.json();
-      })
-      .then(function (data) {
-          if (data["error"]) {
-              this.setState({
-                  error: data["message"]
-              });
-          }
-          else {
-              console.log(data);
-          }
-
-      }.bind(this));
-}
-
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.course);
+    console.log(this.state.bookname);
+  }
+    search() {
+    const bname = this.state.bookname;
+    const course = this.state.course;
+    if(bname || course) {
+     this.props.history.push(`/search?bname=${bname}&course=${course}`);
+     if(this.props.location.pathname === '/search'){
+      window.location.reload();
+        }
+     
+    } else {
+      alert("Please fill at least the book name or course id.")
+    }
+  }
   render() {return (
-    <div className="App">
-        <div className="w-full overflow-hidden text-center flex justify-center">
-          <div className="absolute w-full-w/o-margins  mt-24">
-            <p className="md:text-4xl lg:text-5xl text-lg text-white font-sans font-bold mb-4 md:mb-12">
-Buy and Sell your Books
-                SJSU Bookie's homepage
-            </p>
-            <div className="flex flex-wrap -m-2">
-            </div>
+    <div className="App bg-white">
+        <div className="w-full overflow-hidden text-center flex justify-center font-sans-pro text-xl font-bold">
+        <img src={background} className="h-160 w-full light-filter z-0 object-cover"></img>
+       
+          <div className="absolute mt-48 z-10 w-full flex flex-wrap justify-center">
+          <div class=" flex w-1/2 flex-row flex-wrap justify-center">
+            <input type="text" value={this.state.course} onChange={this.handleChange} id="course" class=" text-gray-600  h-14 w-2/3  p-2 px-4 font-bold  rounded-l-lg" placeholder="Search by Course Name (CS157A)"/>
+            <button class=" p-2  w-1/3 h-14  bg-blue-new rounded-r-lg  font-bold text-white hover:bg-teal-600" onClick={(e) => this.search(e)} type="button">Search by Course</button>
+          </div>
+
         </div>
+        <div className="absolute mt-64 z-10 w-full flex flex-wrap justify-center items-center">
+          <div class=" flex w-1/2 mt-4 flex-row flex-wrap justify-center items-center ">
+          <div onClick={(e) => this.goTo(e)}className="w-3/4 bg-white mt-4 h-20 z-10 cursor-pointer rounded items-center pt-1 border-blue-new border-2">
+              <p className="items-center mt-5 text-4xl font-bold">Click here to sell your Book!!</p>
+          </div>
+</div>
+
+        </div>
+
       </div>
       </div>
   );

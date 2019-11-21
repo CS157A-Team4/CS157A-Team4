8dc0bcd20068ae6eb916e0235e696d2edc03c640
yuxiu198 <xiuyutiffany@gmail.com>
2019-11-21 10:10:27 -0800

    Update profile.js

--- src/Pages/profile/profile.js
+++ src/Pages/profile/profile.js
@@ -38,13 +38,12 @@ class Profile extends React.Component {
 
             <div className="w-0 md:w-1/4 lg:w-1/5 h-0 md:h-screen overflow-y-hidden bg-white shadow-lg">
                 <div className="p-5 bg-white sticky bg-gray-200">
-                    <button className="pt-2 border-t mt-5 w-full text-center text-xl text-gray-600 round-full">
-                        USER
-                    </button>
-                    <button className="hover:bg-gray-300 bg-gray-200 border-t-2 p-3 w-full text-xl text-left text-gray-600 font-semibold"
-                    >Posts</button>
-                    <button className="hover:bg-gray-300 bg-gray-200 border-t-2 p-3 w-full text-xl text-left text-gray-600 font-semibold"
-                    >Settings</button>
+                    <p className="pt-2 border-t mt-5 w-full text-left text-xl text-gray-600 round-full">User Name
+                    </p>
+                    <button className="hover:bg-gray-300 bg-gray-200 border-t-2 p-3 w-full text-xl text-left
+                    text-gray-600 font-semibold">Posts</button>
+                    <button className="hover:bg-gray-300 bg-gray-200 border-t-2 p-3 w-full text-xl text-left
+                    text-gray-600 font-semibold">Settings</button>
                 </div>
                 <div className="w-full h-screen antialiased flex flex-col hover:cursor-pointer">
 

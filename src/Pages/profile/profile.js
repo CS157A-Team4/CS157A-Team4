0029094d9929911499b0a82e351cd22c66434099
yuxiu198 <xiuyutiffany@gmail.com>
2019-11-21 13:31:14 -0800

    Update profile.js

--- src/Pages/profile/profile.js
+++ src/Pages/profile/profile.js
@@ -18,38 +18,25 @@ class Profile extends React.Component {
 	render() {
     	return (
         <div>
-          {/*<div className="App">*/}
-          {/*  <header className="App-header">*/}
-          {/*    <img src={logo} className="App-logo" alt="logo" />*/}
-          {/*    <p>*/}
-          {/*      Bookie's Temporary Profile Page*/}
-          {/*    </p>*/}
-          {/*    <button*/}
-          {/*      className="App-link"*/}
-          {/*      value = ""*/}
-          {/*      onClick={(e) => this.goTo(e)}*/}
-          {/*    >*/}
-          {/*      Click here to return home*/}
-          {/*    </button>*/}
-          {/*  </header>*/}
-          {/*</div>*/}
-
-
-
             <div className="w-0 md:w-1/4 lg:w-1/5 h-0 md:h-screen overflow-y-hidden bg-white shadow-lg">
                 <div className="p-5 bg-white sticky bg-gray-200">
-                    <p className="pt-2 border-t mt-5 w-full text-left text-xl text-gray-600 round-full">User Name
-                    </p>
-                    <button className="hover:bg-gray-300 bg-gray-200 border-t-2 p-3 w-full text-xl text-left
-                    text-gray-600 font-semibold">Posts</button>
-                    <button className="hover:bg-gray-300 bg-gray-200 border-t-2 p-3 w-full text-xl text-left
-                    text-gray-600 font-semibold">Settings</button>
+                    <p className="pt-2 border-t mt-5 w-full text-center text-xl text-gray-900 round-full bg-blue-new
+                    font-sans-pro font-bold">
+                        User Name</p>
                 </div>
-                <div className="w-full h-screen antialiased flex flex-col hover:cursor-pointer">
-
-
+                <div>
+                    <button className="hover:bg-gray-400 bg-gray-200 border-t-2 p-4 w-full text-xl text-left
+                    text-gray-700 font-sans-pro font-bold text-justify">Posts</button>
+                    <button className="hover:bg-gray-400 bg-gray-200 border-t-2 p-4 w-full text-xl text-left
+                    text-gray-700 font-sans-pro font-bold text-justify">Friends</button>
+                    <button className="hover:bg-gray-400 bg-gray-200 border-t-2 p-4 w-full text-xl text-left
+                    text-gray-700 font-sans-pro font-bold text-justify">Settings</button>
                 </div>
-
+            </div>
+            <div className="bg-gray-1000">
+                <div className="text-gray-700 text-center bg-white-400 px-4 py-2 m-2"><button>1</button></div>
+                <div className="text-gray-700 text-center bg-white-400 px-4 py-2 m-2">2</div>
+                <div className="text-gray-700 text-center bg-white-400 px-4 py-2 m-2">3</div>
             </div>
         </div>
 

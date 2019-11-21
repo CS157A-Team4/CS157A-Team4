a0fad01681267121677fec022159915f933d1a69
yuxiu198 <xiuyutiffany@gmail.com>
2019-11-21 07:41:22 -0800

    updates pages

--- src/Pages/friends/friendsList.js
+++ src/Pages/friends/friendsList.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import logo from "../../logo.svg";
 
-/* Create a Friends list and request page with table view
-* reference: https://dev.to/abdulbasit313/an-easy-way-to-create-a-customize-dynamic-table-in-react-js-3igg
-* */
 class Table extends React.Component {
     constructor(props) {
         super(props);
@@ -47,110 +44,50 @@ class Table extends React.Component {
             console.log(friends.surname);
 
             return (
-                <table className="table-auto">
-                    <thead>
-                    <tr>
-                        <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b
-                        border-grey-light">ID</th>
-                        <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b
-                        border-grey-light">User1</th>
-                        <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b
-                        border-grey-light">User2</th>
-                        <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b
-                        border-grey-light">First name</th>
-                        <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b
-                        border-grey-light">Last name</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr key={relationshipId}>
-                        <td className="py-4 px-6 border-b border-grey-light">{relationshipId}</td>
-                        <td className="py-4 px-6 border-b border-grey-light">{user1}</td>
-                        <td className="py-4 px-6 border-b border-grey-light">{user2}</td>
-                        <td className="py-4 px-6 border-b border-grey-light">{firstname}</td>
-                        <td className="py-4 px-6 border-b border-grey-light">{surname}</td>
-                        <td> <button
-                            className="bg-green-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-l-full">
-                            Accept
-                        </button>
-                            {' '}
-                            <button
-                                className="bg-red-300 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-r-full">
-                                Delete
-                            </button></td>
-                    </tr>
-                    </tbody>
-                </table>
-                // <tr key={relationshipId}>
-                //     <td class="py-4 px-6 border-b border-grey-light">{relationshipId}</td>
-                //     <td class="py-4 px-6 border-b border-grey-light">{user1}</td>
-                //     <td class="py-4 px-6 border-b border-grey-light">{user2}</td>
-                //     <td class="py-4 px-6 border-b border-grey-light">{firstname}</td>
-                //     <td class="py-4 px-6 border-b border-grey-light">{surname}</td>
-                // </tr>
+                <tr className="border-b-2 border-aqua " key={relationshipId}>
+                    {/*<td className="py-4 px-6 text-center border-grey-light">{relationshipId}</td>*/}
+                    {/*<td className="py-4 px-6 text-center border-b border-grey-light">{user1}</td>*/}
+                    <td className="py-4 px-6 text-center border-b border-grey-light">{user2}</td>
+                    <td className="py-4 px-6 text-center border-b border-grey-light">{firstname}</td>
+                    <td className="py-4 px-6 text-center border-b border-grey-light">{surname}</td>
+                </tr>
 
             )
         })
     }
 
-    // a function for uppercase the table header ID, USER1, USER2
-    renderFriendsTableHeader() {
-        // object.keys gives the keys of friends in array and stored in variable header
-        let header = Object.keys(this.state.friends[0]);
-        return header.map((key, index) => {
-            return <th key={index}>{key.toUpperCase()}</th>
-            }
-        )
-    }
-
-
-
     render() {
         return(
-            <div>
-                <thead>
-                <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark
-                    border-b border-grey-light" id='title'>Friends list Table</th></thead>
-                    <table className="table-auto" id='friends'>
-                        {/*<thead>*/}
-                        {/*<tr>*/}
-                        {/*    <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark
-                        border-b border-grey-light">ID</th>*/}
-                        {/*    <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark
-                        border-b border-grey-light">User1</th>*/}
-                        {/*    <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark
-                        border-b border-grey-light">User2</th>*/}
-                        {/*    <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark
-                        border-b border-grey-light">first name</th>*/}
-                        {/*    <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark
-                        border-b border-grey-light">last name</th>*/}
-
-                        {/*</tr>*/}
-                        {/*</thead>*/}
-                        <tbody>
+            <div className="font-sans-pro text-2xl">
+                <div className="flex justify-center">
+                    <h1 className="py-4 px-6 bg-grey-lightest font-bold font-sans-pro
+                    border-b border-grey-light">
+                        Friends Forever Table
+                    </h1>
+                </div>
+                <div className="flex justify-center">
+                    <table className="table-fixed" id='friends' >
+                        <thead>
                         <tr>
-                            {this.renderFriendsTable()}
+                        {/*    <th className="py-4 px-6 bg-grey-lightest text-center font-bold font-sans-pro*/}
+                        {/*border-b border-grey-light">ID</th>*/}
+                        {/*    <th className="py-4 px-6 bg-grey-lightest text-center font-bold font-sans-pro*/}
+                        {/*border-b border-grey-light">User1</th>*/}
+                            <th className="py-4 px-6 bg-grey-lightest text-center font-bold font-sans-pro
+                        border-b border-grey-light">Friend Id</th>
+                            <th className="py-4 px-6 bg-grey-lightest text-center font-bold font-sans-pro
+                        border-b border-grey-light">First Name</th>
+                            <th className="py-4 px-6 bg-grey-lightest text-center font-bold font-sans-pro
+                        border-b border-grey-light">Last Name</th>
                         </tr>
+                        </thead>
+                        <tbody>
+                        {this.renderFriendsTable()}
                         </tbody>
-                            {/*<tr>{this.renderFriendsTableHeader()}</tr>*/}
+                        {/*<tr>{this.renderFriendsTableHeader()}</tr>*/}
                     </table>
-
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Bookie's Temporary friends list Page
-                    </p>
-                    <button
-                        className="App-link"
-                        value = ""
-                        onClick={(e) => this.goTo(e)}
-                    >
-                        Click here to return home
-                    </button>
-                </header>
+                </div>
             </div>
-
-
         )
     }
 }
--- src/Pages/profile/profile.js
+++ src/Pages/profile/profile.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import logo from '../../logo.svg';
 
-class Profile extends React.Component {	
+class Profile extends React.Component {
     constructor(props) {
         super(props);
-      }
+        this.state = {
+            posts: [],
+            password: [],
+            loaded:false // do not reload page
+        };
+    }
+
     goTo(event) {
         const value = event.target.value;
         this.props.history.push(`/${value}`);
@@ -12,22 +18,42 @@ class Profile extends React.Component {
 	render() {
     	return (
         <div>
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Bookie's Temporary Profile Page
-              </p>
-              <button
-                className="App-link"
-                value = ""
-                onClick={(e) => this.goTo(e)}
-              >
-                Click here to return home
-              </button>
-            </header>
-          </div>
+          {/*<div className="App">*/}
+          {/*  <header className="App-header">*/}
+          {/*    <img src={logo} className="App-logo" alt="logo" />*/}
+          {/*    <p>*/}
+          {/*      Bookie's Temporary Profile Page*/}
+          {/*    </p>*/}
+          {/*    <button*/}
+          {/*      className="App-link"*/}
+          {/*      value = ""*/}
+          {/*      onClick={(e) => this.goTo(e)}*/}
+          {/*    >*/}
+          {/*      Click here to return home*/}
+          {/*    </button>*/}
+          {/*  </header>*/}
+          {/*</div>*/}
+
+
+
+            <div className="w-0 md:w-1/4 lg:w-1/5 h-0 md:h-screen overflow-y-hidden bg-white shadow-lg">
+                <div className="p-5 bg-white sticky bg-blue-500">
+                    <button className="pt-2 border-t mt-5 w-full text-center text-xl text-gray-600 round-full">
+                        USER
+                    </button>
+                    <button className="hover:bg-gray-300 bg-gray-200 border-t-2 p-3 w-full text-xl text-left text-gray-600 font-semibold"
+                    >Posts</button>
+                    <button className="hover:bg-gray-300 bg-gray-200 border-t-2 p-3 w-full text-xl text-left text-gray-600 font-semibold"
+                    >Settings</button>
+                </div>
+                <div className="w-full h-screen antialiased flex flex-col hover:cursor-pointer">
+
+
+                </div>
+
+            </div>
         </div>
+
 		)	
 	}
 }

8491dda9bf5e49da9f378aac746ba947b5528f9d
jvan1997 <jonathanvan.1997@gmail.com>
2019-12-10 14:27:24 -0800

    Update src/backend/backend.js

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -1,9 +1,6 @@
 
 // switch back and forth when you want to test
-const api = process.env.GATSBY_backendPoint ? process.env.GATSBY_backendPoint : "localhost:3000";
-console.log(process.env.GATSBY_backendPoint);
-console.log(process.env);
+const api = process.env.NODE_ENV === "production" ? "https://sjsubookiebackend.herokuapp.com" : "localhost:3000";
 //const api = "https://sjsubookietest.herokuapp.com";
 //const api = "https://sjsubookielogin.herokuapp.com"
-console.log(api);
 export default api

5f4781bf6f37f86d43697a47af758597fede8046
jvan1997 <jonathanvan.1997@gmail.com>
2019-12-10 14:10:56 -0800

    changes between apis

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -1,7 +1,7 @@
 
 // switch back and forth when you want to test
-const api = "https://sjsubookiebackend.herokuapp.com";
+const api = process.env.GATSBY_backendPoint? process.env.GATSBY_backendPoint : "localhost:3000";
 //const api = "https://sjsubookietest.herokuapp.com";
 //const api = "https://sjsubookielogin.herokuapp.com"
-
+console.log(api);
 export default api

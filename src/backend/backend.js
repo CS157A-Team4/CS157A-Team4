9f0b90eb2984df79259736edfc7e0b76ec8154a6
jvan1997 <jonathanvan.1997@gmail.com>
2019-12-10 14:16:08 -0800

    Update src/backend/backend.js

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -1,6 +1,7 @@
 
 // switch back and forth when you want to test
-const api = process.env.GATSBY_backendPoint? process.env.GATSBY_backendPoint : "localhost:3000";
+const api = process.env.GATSBY_backendPoint ? process.env.GATSBY_backendPoint : "localhost:3000";
+console.log(process.env.GATSBY_backendPoint);
 //const api = "https://sjsubookietest.herokuapp.com";
 //const api = "https://sjsubookielogin.herokuapp.com"
 console.log(api);
